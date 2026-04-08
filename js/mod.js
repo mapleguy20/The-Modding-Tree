6180f6b1c3fc79f759a45913c16f3be714f636c3
mapleguy20 <williambacon404@gmail.com>
2026-04-08 19:24:47 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	author: "nobody",
-	pointsName: "points",
+	name: "The maple Tree",
+	author: "MapleGuy20",
+	pointsName: "Maple Fragments",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
